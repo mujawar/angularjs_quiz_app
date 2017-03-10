c39f12378f498b686d732ba8acc393f1139daf45
arif <arifmr91@gmail.com>
2017-03-11 00:23:14 +0530

    second commit

---
+++ js/controllers/list.js
@@ -0,0 +1,11 @@
+(function () {
+    angular
+        .module('myApp')
+        .controller('listCtrl',ListController);
+    
+    
+        function ListController() {
+            
+        }
+
+})();
