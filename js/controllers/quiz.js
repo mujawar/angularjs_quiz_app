cee9a2953e487c21a5989924b023695f9fb6c4bb
arif <arifmr91@gmail.com>
2017-03-16 00:06:57 +0530

    18th commit

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -15,29 +15,61 @@
             vm.DataService = DataService;
             vm.questionAnswered = questionAnswered;
             vm.setActiveQuestion = setActiveQuestion;
-            vm.avtiveQuestion = 0;
+            vm.selectAnswer = selectAnswer;
+            vm.activeQuestion = 0;
             var numQuestionAnswered = 0;
             var quizLength = DataService.quizQuestions.length;
+            vm.error = false;
+            vm.finalize = false;
+            vm.finalizeAnswer = finalizeAnswer;
 
-            function setActiveQuestion() {
-                var breakOut =false;
-                var quizLengthsas = DataService.quizQuestions.length-1;
-                while(!breakOut){
-                    vm.avtiveQuestion = vm.avtiveQuestion < quizLengthsas?++vm.avtiveQuestion:0;
-                    if(DataService.quizQuestions[vm.avtiveQuestion].selected === null){
-                        breakOut = true;
+            function setActiveQuestion(index) {
+                if(index === undefined){
+                    var breakOut =false;
+                    var quizLengthsas = DataService.quizQuestions.length-1;
+                    while(!breakOut){
+                        vm.activeQuestion = vm.activeQuestion < quizLengthsas?++vm.activeQuestion:0;
+                        if( vm.activeQuestion === 0){
+                            vm.error = true;
+                        }
+                        if(DataService.quizQuestions[vm.activeQuestion].selected === null){
+                            breakOut = true;
+                        }
                     }
+                }else{
+                    vm.activeQuestion = index;
                 }
             }
 
             function questionAnswered() {
-                if(DataService.quizQuestions[vm.avtiveQuestion].selected !== null){
+                if(DataService.quizQuestions[vm.activeQuestion].selected !== null){
                     numQuestionAnswered++;
                     if(numQuestionAnswered >= quizLength){
-                        //
+                        for(var i = 0 ; i <quizLength ; i++){
+                            if(DataService.quizQuestions[i].selected === null){
+                                setActiveQuestion(i);
+                                return;
+                            }
+                        }
+                        vm.error = false;
+                        vm.finalize = true;
+                        return;
                     }
                 }
                 vm.setActiveQuestion();
+            }
+            function selectAnswer(index) {
+                DataService.quizQuestions[vm.activeQuestion].selected = index;
+            }
+
+            function finalizeAnswer() {
+                vm.finalize = false;
+                numQuestionAnswered = 0;
+                vm.activeQuestion = 0;
+                quizMetrics.markQuiz();
+                quizMetrics.changeState("quiz",false);
+                quizMetrics.changeState("results",true);
+
             }
         }
         
