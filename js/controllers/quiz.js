fe39154e1b208ed99e99ea040b1537f48cf38005
arif <arifmr91@gmail.com>
2017-03-15 15:44:20 +0530

    13th commit

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -7,7 +7,41 @@
             .module('myApp')
             .controller('quizCtrl', QuizController);
 
-        function QuizController() {
+    QuizController.$inject = ['quizMetrics','DataService'];
+
+        function QuizController(quizMetrics,DataService) {
             var vm = this;
+            vm.quizMetrics =quizMetrics;
+            vm.DataService = DataService;
+            vm.questionAnswered = questionAnswered;
+            vm.setActiveQuestion = setActiveQuestion;
+            vm.avtiveQuestion = 0;
+            var numQuestionAnswered = 0;
+            var quizLength = DataService.quizQuestions.length;
+
+            function setActiveQuestion() {
+                var breakOut =false;
+                var quizLengthsas = DataService.quizQuestions.length-1;
+                while(!breakOut){
+                    vm.avtiveQuestion = vm.avtiveQuestion < quizLengthsas?++vm.avtiveQuestion:0;
+                    if(DataService.quizQuestions[vm.avtiveQuestion].selected === null){
+                        breakOut = true;
+                    }
+                }
+            }
+
+            function questionAnswered() {
+                if(DataService.quizQuestions[vm.avtiveQuestion].selected !== null){
+                    numQuestionAnswered++;
+                    if(numQuestionAnswered >= quizLength){
+                        //
+                    }
+                }
+                vm.setActiveQuestion();
+            }
         }
+        
+        
+
+
     })();
